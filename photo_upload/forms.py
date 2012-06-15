13db0e05aa6e446b30cc9f983c89dd766b20dda9
Mike Vattuone <mvattuone@gmail.com>
2012-06-15 16:11:23 -0700

    attempting to use PhotoForms rather than returning request.POST in context, as that doesn't really make any sense at all, since we're talking about a file that we want to load after it has been uploaded.

--- photo_upload/forms.py
+++ photo_upload/forms.py
@@ -2,9 +2,13 @@ from django.forms import ModelForm
 from django import forms
 from photo_upload.models import *
 
+class RawPhotoForm(ModelForm):
+	class Meta:
+		model = Photo
+		fields = {'raw_photo',}
+
 class PhotoForm(ModelForm):
 	class Meta:
 		model = Photo
-		exclude = {'approved',}
-		
+		exclude = {'approved','raw_photo'}
 		
--- photo_upload/views.py
+++ photo_upload/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
-
+from django.http import HttpResponseRedirect
 from photo_upload.models import *
 
-from photo_upload.forms import PhotoForm
+from photo_upload.forms import *
 	
 
 def index(request):
@@ -25,21 +25,25 @@ def index(request):
 	
 @csrf_exempt
 def save_raw_image(request):
-	raw_post = request.POST
 	textbox = TextBox.objects.all()
 	context = {
-		"form": PhotoForm(),
+		"form": RawPhotoForm(),
 		"photos": Photo.objects.filter(approved=True),
 		"textbox": textbox,
-		"raw_post": raw_post,
 	}
 	if request.method == 'POST':
-		print request.POST
+		#I thought certain file types were returned as request.FILE, rather than POST.  This part is still a bit mysterious to me.
+		form = RawPhotoForm(request.POST, request.FILES)
+		if form.is_valid():
+			new_upload = form.save(commit=False)
+			return HttpResponseRedirect("raws/")
+		else:
+			context['form'] = form
+			print form.errors
 		#Save object reference to new Photo instance, with the property raw_photo only, and upload file to server in raw directory.  We want to pass this back too, so I assume that would be something we add to the context...
 	else:
 		print "Error"
 	return render_to_response("index.html",context,context_instance=RequestContext(request))
-		#TODO: is there a better way to return this to the same page but with an updated context?
 			
 			
 			
