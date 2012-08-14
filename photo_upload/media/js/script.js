422fe1e780f17e2f6b966e0cb11e7900f14c7115
Josh Levinger <josh.l@engagementlab.org>
2012-08-13 18:37:59 -0700

    basic photo uploading now works, add error states

--- photo_upload/media/js/script.js
+++ photo_upload/media/js/script.js
@@ -2,7 +2,7 @@
 
 webcam.set_swf_url('../static/swf/webcam.swf');
 webcam.set_shutter_sound(true, "../static/mp3/shutter.mp3");
-webcam.set_quality(90);
+webcam.set_quality(95);
 webcam.set_stealth(false);
 webcam.set_hook('onComplete', 'callbackCamera');
     
@@ -11,40 +11,38 @@ $("#camera").html(webcam.get_html(350, 260));
 
 //Switches uploads steps
 function switchStep() {
-	$("#step-1").hide();
-	$("#step-2").show();
+    $("#step-1").hide();
+    $("#step-2").show();
 }
 
 //Callback function for jpegcam - invokes switchStep, creates canvas, and adds jpeg image to canvas
 function callbackCamera(response) {
-	var data = JSON.parse(response);
-	//going from step 1 to 2 in the upload form.
-	switchStep();
-	//canvas 
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
+    var data = JSON.parse(response);
+    //going from step 1 to 2 in the upload form.
+    switchStep();
+    //canvas
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
 
-	//Making an image object to draw to my canvas
+    //Making an image object to draw to my canvas
+    var img = new Image();
+    img.src = data.file_url;
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, 350, 260);
+    };
+    
+    $("#id_name").change(function() {
+        ctx.drawImage(img, 0, 0, 350, 260);
+        ctx.font = "36px Helvetica";
+        ctx.fillText(this.value, 0, 80);
+    });
+    
+    $("#id_zip_code").change(function() {
+        ctx.drawImage(img, 0, 0, 350, 260);
+        ctx.font = "24px Helvetica";
+        ctx.fillText(this.value, 0, 150);
+    });
 
-		var img = new Image();
-		img.src = data.file_url
-		img.onload = function() {
-			ctx.drawImage(img, 0, 0, 350, 260);
-		
-	} 
-	
-	$("#id_name").change(function() {
-		ctx.drawImage(img, 0, 0, 350, 260);
-		ctx.font = "36px Helvetica"
-		ctx.fillText(this.value, 0, 80);
-	});
-	
-	$("#id_zip_code").change(function() {
-		ctx.drawImage(img, 0, 0, 350, 260);
-		ctx.font = "24px Helvetica"
-		ctx.fillText(this.value, 0, 150);
-	});
-	
 }
 
 $('#tab').click(function(e){
@@ -52,33 +50,58 @@ $('#tab').click(function(e){
 });
 
 $("#snap-button").click(function() {
-	webcam.snap("upload_raw_photo", "callbackCamera");	
+    webcam.snap("upload_raw_photo", "callbackCamera");
 });
 
 //Show upload form field for photo
 $("#show_photo").click(function() {
-	document.location.hash = "#show_photo"
-	if(document.location.hash === "#show_photo") {
-		$("div.nophoto").removeClass("nophoto");
-		$("div#captioned_photo").hide();
-		$("#step-1").hide();
-		$("#step-2").show();
-	document.location.hash = "#upload_photo"
+    document.location.hash = "#show_photo";
+    if(document.location.hash === "#show_photo") {
+        $("div.nophoto").removeClass("nophoto");
+        $("div#captioned_photo").hide();
+        $("#step-1").hide();
+        $("#step-2").show();
+    document.location.hash = "#upload_photo";
 }
 });
 
+/*
 var addToCanvas = function(text) {
-	ctx.fillStyle = "#000";
-	ctx.font = "40px Helvetica";
-	ctx.globalAlpha=1;
-	ctx.fillText(text, 40,80)
-}
+    ctx.fillStyle = "#000";
+    ctx.font = "40px Helvetica";
+    ctx.globalAlpha=1;
+    ctx.fillText(text, 40,80);
+}*/
 
 
 //TODO: Connect this to same submit as other submit request
 $("#sendForm").click(function(e) {
-	e.preventDefault();
-	var canvas = document.getElementById("canvas");
-	var dataURL = canvas.toDataURL();
-	$.post("/submit", {captioned_photo: dataURL});
+    e.preventDefault();
+    var canvas = document.getElementById("canvas");
+    var dataURL = canvas.toDataURL();
+
+    $('input').removeClass('error');
+    $('label').removeClass('error');
+
+    $.ajax({type: 'POST',
+        url:"/submit",
+        contentType: "application/json",
+        dataType: "json",
+        data: {captioned_photo: dataURL,
+         name:$('#id_name').val(),
+         zip_code: $('#id_zip_code').val(),
+         email: $('#id_email').val(),
+         raw_photo_id: $('#id_raw_photo').val()
+        },
+        error: function(jqXHR, textStatus) {
+            console.log('error');
+            var errors = $.parseJSON(jqXHR.responseText);
+            console.log(errors);
+            $('input#id_'+errors.field).addClass('error');
+            $('label[for="id_'+errors.field+'"]').addClass('error');
+        },
+        success: function(jqXHR, textStatus, errorThrown) {
+            console.log('success');
+        }
+    });
 });
--- photo_upload/views.py
+++ photo_upload/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseBadRequest
 from django.core.files.base import ContentFile
 from django.conf import settings
 from django.shortcuts import render
 import json
 
+import re
+from django.core.files.base import ContentFile
+import tempfile
+
 from photo_upload.models import *
 from photo_upload.forms import *
-	
+    
 
 def index(request):
-	
     textbox = TextBox.objects.all()
     form = PhotoForm(request.POST or None)
     if form.is_valid():
@@ -36,26 +39,48 @@ def index(request):
     }
 
     return render(request, "index.html", dictionary=context)
-	
+    
 @csrf_exempt
 def upload_raw_photo(request):
-	if request.method == 'POST':
- 		raw_photo = RawPhoto()
- 		raw_content_file = ContentFile(request.raw_post_data)
- 		file_name = "raw_photo.png"
- 		raw_photo.photo.save(file_name, raw_content_file)
- 		data = {
- 			'success': True,
- 			'file_name': file_name,
- 			'file_url': raw_photo.photo.url,
- 			'raw_photo_pk': raw_photo.pk
- 		}
- 	else:
- 		data = {'success': False}
-	return HttpResponse(json.dumps(data))
-	
+    if request.method == 'POST':
+        raw_photo = RawPhoto()
+        raw_content_file = ContentFile(request.raw_post_data)
+        file_name = "raw_photo.png"
+        raw_photo.photo.save(file_name, raw_content_file)
+        data = {
+            'success': True,
+            'file_name': file_name,
+            'file_url': raw_photo.photo.url,
+            'raw_photo_pk': raw_photo.pk
+        }
+    else:
+        data = {'success': False}
+    return HttpResponse(json.dumps(data),mimetype="application/json")
+    
 @csrf_exempt
 def submit(request):
-	print request
+    required_fields = ['name','zip_code','email','captioned_photo']
+    for f in required_fields:
+        if request.POST.get(f) == "":
+            resp = {'message':'%s is required' % f,
+                    'field':f}
+            return HttpResponseBadRequest(json.dumps(resp),mimetype="application/json")
 
-			
+    #decode the dataurl
+    dataurl = request.POST['captioned_photo']
+    encoded_photo = re.search(r'base64,(.*)', dataurl).group(1)
+    decoded_photo = encoded_photo.decode('base64')
+
+    #save it to a ContentFile
+    captioned_content_file = ContentFile(decoded_photo)
+    captioned_file_name = "test_upload.png"
+
+    #create the django photo object
+    new_photo = Photo.objects.create(name=request.POST.get('name'),
+                                    zip_code=request.POST.get('zip_code'),
+                                    email=request.POST.get('email'))
+    new_photo.captioned_photo.save(captioned_file_name,captioned_content_file)
+    new_photo.save()
+
+    resp = {'message':'success'}
+    return HttpResponse(json.dumps(resp),mimetype="application/json")
