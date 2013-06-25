ce6c13e4a9276fa6ce186932cd78f158f29ac20c
Mike Vattuone <mvattuone@gmail.com>
2013-06-24 18:20:41 -0700

    remove usps app

--- usps/api/__init__.py
+++
@@ -1,3 +0,0 @@
-USPS_CONNECTION = 'http://production.shippingapis.com/ShippingAPI.dll'
-USPS_CONNECTION_TEST_SECURE = 'https://secure.shippingapis.com/ShippingAPITest.dll'
-USPS_CONNECTION_TEST = 'http://testing.shippingapis.com/ShippingAPITest.dll'
